e894d2c8d84d12ae6659f9bfc7ab473f25f2f20e
luggamugga <l.ongyerth@gmail.com>
2023-12-13 16:38:15 +0100

    fix login

--- login.php
+++ login.php
@@ -1,4 +1,10 @@
+<?php
+if(!empty($_POST["usrname"])){
+    echo login($_POST["usrname"],$_POST["passwd"]);
+}
+?>
 <?php include "header.php" ?>
+
 <div class="loginForm">
     <form method="post">
         <label for="usrname">Username:</label>
@@ -7,10 +13,6 @@
         <input type="password" name="passwd">
         <input type="submit">
     </form></div>
-    <?php
-    if(!empty($_POST["usrname"])){
-       echo login($_POST["usrname"],$_POST["passwd"]);
-    }
-    ?>
+
 
 <?php include "footer.php" ?>
