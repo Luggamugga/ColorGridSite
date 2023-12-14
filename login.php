9d05b1f34d38de8ccb853672b08ac9a2c2dd7534
luggamugga <l.ongyerth@gmail.com>
2023-12-14 12:28:43 +0100

    fix login

--- login.php
+++ login.php
@@ -1,24 +1,34 @@
 <?php
-if(!empty($_POST["usrname"])){
-    if (login($_POST["usrname"],$_POST["passwd"])){
-        header("location:logged.php");
-    } else {
-        echo "wrong username or password";
-    }
 
-
-}
+include "header.php" ;
 ?>
-<?php include "header.php" ?>
-
-<div class="loginForm">
-    <form method="post">
+<?php
+$logged = false;
+function renderLogin(){
+    $returnarr[] = "<div class='loginForm'>";
+    $returnarr[] = '<form method="post">
         <label for="usrname">Username:</label>
         <input type="text" name="usrname">
         <label for="passwd">Password:</label>
         <input type="password" name="passwd">
         <input type="submit">
-    </form></div>
-
+    </form>';
+    $returnarr[] = "</div>";
+    return implode("",$returnarr);
+}
+function renderLogged(){
+    return include "logged.php";
+}
+if(!empty($_POST["usrname"])){
+    if (login($_POST["usrname"],$_POST["passwd"])){
+        echo renderLogged();
+    } else {
+        echo "wrong username or password";
+    }
+    unset($_POST);
+} else{
+    echo renderLogin();
+}
+?>
 
 <?php include "footer.php" ?>
